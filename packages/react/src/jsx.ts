cbe38dcfed1c73a253745239d13082286df307ee
xzw <1272117264@qq.com>
2023-11-29 17:00:10 +0800

    feat: 实现jsx方法

---
+++ packages/react/src/jsx.ts
@@ -0,0 +1,91 @@
+import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbols';
+import {
+	Props,
+	Key,
+	Ref,
+	ReactElementType,
+	ElementType
+} from 'shared/ReactTypes';
+
+const ReactElement = function (
+	type: ReactElementType,
+	key: Key,
+	ref: Ref,
+	props: Props
+) {
+	const element = {
+		$$typeof: REACT_ELEMENT_TYPE,
+		type,
+		key,
+		ref,
+		props,
+		__mark: 'xzw'
+	};
+
+	return element;
+};
+
+export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
+	let key: Key = null;
+	let ref: Ref = null;
+	const props: Props = {};
+
+	for (const prop in config) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+
+	const maybeChildrenLength = maybeChildren.length;
+	if (maybeChildren > 0) {
+		if (maybeChildrenLength === 1) {
+			props.children = maybeChildren[0];
+		} else {
+			props.children = maybeChildren;
+		}
+	}
+
+	return ReactElement(type, key, ref, props);
+};
+
+export const jsxDEV = (type: ElementType, config: any) => {
+	let key: Key = null;
+	let ref: Ref = null;
+	const props: Props = {};
+
+	for (const prop in config) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+
+	return ReactElement(type, key, ref, props);
+};
