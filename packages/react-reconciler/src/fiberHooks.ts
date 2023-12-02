c657312477ad29560fe5b8dbc24488b6fb73b2fe
xzw <1272117264@qq.com>
2023-12-02 14:58:46 +0800

    feat: 实现useState

--- packages/react-reconciler/src/fiberHooks.ts
+++ packages/react-reconciler/src/fiberHooks.ts
@@ -1,9 +1,105 @@
+import { Dispatch, Dispatcher } from 'react/src/currentDIspatcher';
 import { FiberNode } from './fiber';
+import internals from 'shared/internals';
+import {
+	UpdateQueue,
+	createUpdate,
+	createUpdateQueue,
+	enqueueUpdate
+} from './updateQueue';
+import { Action } from 'shared/ReactTypes';
+import { scheduleUpdateOnFiber } from './workLoop';
+// hooks linked list
+interface Hook {
+	memoizedState: any;
+	updateQueue: unknown;
+	next: Hook | null;
+}
+
+const { currentDispatcher } = internals;
+
+let currentlyRenderingFiber: FiberNode | null = null;
+let workInProgressHook: Hook | null = null;
 
 export const renderWithHooks = (wip: FiberNode) => {
+	// set val
+	currentlyRenderingFiber = wip;
+	// reset
+	wip.memoizedState = null;
+	const current = wip.alternate;
+
+	if (current !== null) {
+		// update
+	} else {
+		currentDispatcher.current = HooksDispatcherOnMount;
+	}
+
 	const Component = wip.type;
 	const props = wip.pendingProps;
 	const children = Component(props);
 
+	// reset
+	currentlyRenderingFiber = null;
 	return children;
 };
+
+const HooksDispatcherOnMount: Dispatcher = {
+	useState: mountState
+};
+
+function mountState<State>(
+	initialState: () => State | State
+): [State, Dispatch<State>] {
+	// 找到当前useState对应hook数据
+	const hook = mountWorkInProgressHook();
+	let memoizedState;
+	if (initialState instanceof Function) {
+		memoizedState = initialState();
+	} else {
+		memoizedState = initialState;
+	}
+
+	const queue = createUpdateQueue<State>();
+	hook.updateQueue = queue;
+	hook.memoizedState = memoizedState;
+
+	// @ts-ignore
+	const dispatch = dispatchSetState.bind(null, currentlyRenderingFiber, queue);
+	queue.dispatch = dispatch;
+
+	return [memoizedState, dispatch];
+}
+
+function dispatchSetState<State>(
+	fiber: FiberNode,
+	updateQueue: UpdateQueue<State>,
+	action: Action<State>
+) {
+	const update = createUpdate(action);
+	enqueueUpdate(updateQueue, update);
+	scheduleUpdateOnFiber(fiber);
+}
+
+function mountWorkInProgressHook() {
+	const hook: Hook = {
+		memoizedState: null,
+		updateQueue: null,
+		next: null
+	};
+
+	if (workInProgressHook === null) {
+		// mount时第一个hook
+		if (currentlyRenderingFiber === null) {
+			throw new Error('请在函数组件内调用hook');
+		} else {
+			workInProgressHook = hook;
+			currentlyRenderingFiber.memoizedState = workInProgressHook;
+		}
+	} else {
+		// mount时 后续hook
+		workInProgressHook.next = hook;
+		workInProgressHook = hook;
+	}
+
+	return workInProgressHook;
+}
--- scripts/rollup/react-dom.config.js
+++ scripts/rollup/react-dom.config.js
@@ -2,13 +2,11 @@ import { getPackageJSON, resolvePkgPath, getBaseRollupPlugins } from './utils';
 import generatePackageJSON from 'rollup-plugin-generate-package-json';
 import alias from '@rollup/plugin-alias';
 
-const { name, module } = getPackageJSON('react-dom');
+const { name, module, peerDependencies } = getPackageJSON('react-dom');
 // 包路径
 const pkgPath = resolvePkgPath(name);
 // 产物路径
 const pkgDistPath = resolvePkgPath(name, true);
-
-console.log('....', ` ${pkgPath}/${module}`);
 
 export default [
 	// react-dom
@@ -26,6 +24,7 @@ export default [
 				format: 'umd'
 			}
 		],
+		external: [...Object.keys(peerDependencies)],
 		plugins: [
 			...getBaseRollupPlugins(),
 			// resolve imprt hostConfig
